ef15b5d0fee08ab2d5a84bc95d4c695b416212b7
Inder-Pal-github <inder39811@gmail.com>
2022-12-21 13:10:42 +0530

    signup UI

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
-}
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const navigate = useNavigate("");
 
-export default Signup
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (username.trim() && password.trim() && email.trim()) {
+      console.log(email, username, password);
+      setPassword("");
+      setUsername("");
+      setEmail("");
+    }
+  };
+  return (
+    <main className="signup">
+      <form className="signup_form" onSubmit={handleSubmit}>
+        <h2 className="signup_title">Create an account</h2>
+        <label htmlFor="email">Email Address</label>
+        <input
+          type="email"
+          id="email"
+          className="email"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          className="password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className="signupButton">REGISTER</button>
+        <p style={{ textAlign: "center", marginTop: "30px" }}>
+          Already have an account?
+          <Link className="link" to="/">
+            Sign in
+          </Link>
+        </p>
+      </form>
+    </main>
+  );
+};
+
+export default Signup;
